67a027d5b849974d5310d2a30f95c5f6986e0e3b
Joshua Benuck <joshua_benuck@yahoo.com>
2020-02-06 13:57:38 -0800

    Changes from second session

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
 const { ErrorKind, DenoError, args, stat, readDir, open, exit } = Deno;
 import { serve } from "https://deno.land/std@v0.30.0/http/server.ts";
 const s = serve({ port: 8000 });
-console.log("http://localhost:8000/");
+console.log("http://localhost:8000/", s);
+
 for await (const req of s) {
+    let headers = new Headers();
+    headers.set("access-control-allow-origin", "*");
+    headers.set(
+        "access-control-allow-headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Range"
+    );
     console.log(req.url)
     if (req.url.indexOf("/hello") == 0) {
         let data = {
@@ -15,12 +22,6 @@ for await (const req of s) {
                 }
             ]
         }
-        let headers = new Headers();
-        headers.set("access-control-allow-origin", "*");
-        headers.set(
-            "access-control-allow-headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Range"
-        );
         req.respond({
             status: 200,
             body: JSON.stringify(data, null, 2),
@@ -28,12 +29,46 @@ for await (const req of s) {
         });
 
     }
-    else if (req.url.indexOf("/favicon.png") == 0) {
-        let filePath = "./favicon.png"
+    else if (req.url.match("/[a-z-]+.json")) {
+        let parts = req.url.split(".json")
+        let path = parts[0].substring(1, parts[0].length).replace("-", "/")
+        let fileInfo = stat(path)
+
+        let data = {
+            title: path,
+            story: [
+                {
+                    type: "code",
+                    text: JSON.stringify(fileInfo, null, 2),
+                    id: "ab35d" 
+                }
+            ]
+        }
+        req.respond({
+            status: 200,
+            body: JSON.stringify(data, null, 2),
+            headers
+        });
+
+    }
+    else if (req.url.match(/^\/.*\.png$/)) {
+        let filePath = `.${req.url}`
         const [file, fileInfo] = await Promise.all([open(filePath), stat(filePath)]);
-        const headers = new Headers();
         headers.set("content-length", fileInfo.len.toString());
         headers.set("content-type", "image/png");
+
+        const res = {
+            status: 200,
+            body: file,
+            headers
+        };
+        req.respond(res);
+    }
+    else if (req.url.indexOf("/index.html") == 0) {
+        let filePath = "./index.html"
+        const [file, fileInfo] = await Promise.all([open(filePath), stat(filePath)]);
+        headers.set("content-length", fileInfo.len.toString());
+        headers.set("content-type", "text/html");
 
         const res = {
             status: 200,
