d61d30a4590ddff4fb9c422bc096e1e6835663d0
Joshua Benuck <joshua_benuck@yahoo.com>
2020-03-08 08:41:19 -0700

    Experimental static wiki link support

--- client/link.mjs
+++ client/link.mjs
@@ -12,6 +12,25 @@ export class Link extends HTMLElement {
         shadow.appendChild(style)
 
         this.anchor = document.createElement("a")
+        let href = this.getAttribute("href")
+        // Convert absolute links to sites in the neighborhood to remote wiki links
+        if (href) {
+            console.log("url", href)
+            let url = new URL(href)
+            let origin = url.origin.replace("http://", "")
+            // This is going to need some more work
+            if (this.wiki.neighborhood.has(origin) && url.pathname.match(/^\/view\/[a-z0-9-]+$/)) {
+                this.setAttribute("site", origin)
+                this.setAttribute("slug", url.pathname.replace("/view/", ""))
+                this.removeAttribute("href")
+                this.removeAttribute("target")
+            }
+            else {
+                this.anchor.setAttribute("href", href)
+                this.anchor.setAttribute("target", this.getAttribute("target"))
+            }
+        }
+
         this.replaceMode()
         this.addEventListener("mousemove", this._updateMode)
         this.addEventListener("click", this.click)
@@ -35,6 +54,9 @@ export class Link extends HTMLElement {
     }
 
     _updateAnchor(url) {
+        if (this.getAttribute("href")) {
+            return
+        }
         let site = this.getAttribute("site")
         let slug = this.getAttribute("slug")
         if (site) {
