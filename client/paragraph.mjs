5be4d245272776752a1bd991562fbeaf526fc8d3
Joshua Benuck <joshua_benuck@yahoo.com>
2020-03-09 17:50:59 -0700

    WIP: Beginnings of edit support

--- client/paragraph.mjs
+++ client/paragraph.mjs
@@ -7,20 +7,43 @@ export class Paragraph extends HTMLElement {
             :host {
                 display: block;
             }
+
+            textarea {
+                width: 100%;
+                font-size: inherit;
+                min-height: 150px;
+            }
         `
         let style = document.createElement("style")
         style.innerHTML = css
         shadow.appendChild(style)
-        let p = document.createElement("p")
+        this.p = document.createElement("p")
         let slot = document.createElement("slot")
-        p.appendChild(slot)
-        shadow.appendChild(p)
+        this.p.appendChild(slot)
+        shadow.appendChild(this.p)
 
         let site = this.page.getAttribute("site")
         this.innerHTML = renderLinks(this.innerHTML, site)
         if (!this.page.dynamic) {
+            this.addEventListener("dblclick", this.edit)
+            this.addEventListener("click", (e) => e.stopPropagation())
+            this.editor = document.createElement("textarea")
+            this.editor.style.display = "none"
+            shadow.appendChild(this.editor)
             this.setAttribute("editable", true)
         }
+    }
+
+    edit(event) {
+        // already editing
+        if (this.editor.style.display == "") {
+            return
+        }
+        this.p.style.display = "none"
+        this.editor.style.display = ""
+        this.editor.value = this.textContent
+        console.log("edit", event, this.editor)
+        this.editor.focus()
     }
 
     get page() {
