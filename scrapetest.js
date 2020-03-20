1bb86261554f2e24fc8a271eb4ecba28ddf55b3e
Joshua Benuck <joshua_benuck@yahoo.com>
2020-03-19 21:19:28 -0700

    Delete files already moved to scripts folder

--- scrapetest.js
+++
@@ -1,4 +0,0 @@
-let t0 = Date.now()
-let action = Deno.args[0]||'state'
-let url = `http://scrape.localtest.me:8000/button?action=${action}`
-console.log(action, '⇒', await fetch(url).then(r=>r.json()), Date.now()-t0, 'ms')
