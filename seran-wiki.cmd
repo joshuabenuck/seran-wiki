e33c6836d4e00d6ec72c1114dba22f9d922ffe6f
Ward Cunningham <ward@c2.com>
2020-04-09 16:33:18 -0700

    remember to keep .sh and .cmd in sync

--- seran-wiki.cmd
+++ seran-wiki.cmd
@@ -1,1 +1,1 @@
-deno -c tsconfig.json --importmap=import_map.json --allow-net --allow-read --allow-write --allow-env ./server/seran.ts %*
+deno -c tsconfig.json --importmap=import_map.json --allow-net --allow-read --allow-write --allow-env --allow-run ./server/seran.ts %*
