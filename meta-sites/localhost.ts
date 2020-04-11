19e7dc11f8645165453922111616e110046da253
Joshua Benuck <joshua_benuck@yahoo.com>
2020-04-11 12:22:54 -0700

    remove meta-pages directory and clean up localhost.ts

--- meta-sites/localhost.ts
+++ meta-sites/localhost.ts
@@ -2,82 +2,94 @@ const { stat } = Deno;
 import * as wiki from "seran/wiki.ts";
 import { Request } from "seran/wiki.ts";
 import { System, MetaSite } from "seran/system.ts";
-
-export let plugins = ["/client/wander.mjs"]
-
-async function readDir(path) {
-  let fileInfo = await stat(path);
-  if (!fileInfo.isDirectory()) {
-    console.log(`path ${path} is not a directory.`);
-    return [];
-  }
-
-  return await Deno.readdir(path);
-}
+import { readFileStr, exists, readJson, writeJson } from "std/fs/mod.ts";
+import {
+  isAbsolute,
+  join,
+  basename
+} from "std/path/posix.ts";
 
 let metaPages = {};
 
-// since constructors cannot be async and readDir is async, use an init method
 export async function init({site, system}: {site: MetaSite, system: System}) {
   wiki.enableLogin(site, system)
-  for (let metaPagePath of await readDir("./meta-pages")) {
-    let metaPage = await import(`../meta-pages/${metaPagePath.name}`);
-    let exports = Object.keys(metaPage);
-    if (exports.length > 1) {
-      console.log(
-        `Warning: Only registering first export of ${metaPagePath.name}`
-      );
-    } else if (exports.length == 0) {
-      console.log(
-        `Warning: Unable to register meta-page for ${metaPagePath.name}`
-      );
-      continue;
-    }
-    metaPages[`/${metaPagePath.name.replace(/\.[tj]s$/, "")}.json`] = metaPage
-      [exports[0]];
-  }
 }
 
-export async function serve(req: Request, system: System) {
-  if (req.url == "/welcome-visitors.json") {
-    wiki.serveJson(
-      req,
-      wiki.welcomePage("[[DenoWiki]]", "[[Admin]], [[Wander]], [[Deno Sites]]")
+export async function sites(req, system) {
+  let sites = [];
+  for (let metaSite of system.metaSitesInDomain(req)) {
+    sites.push(
+      wiki.reference(
+        metaSite,
+        "welcome-visitors",
+        "Welcome Visitors",
+        await doAndShare(metaSite)
+      )
     );
-  } else if (req.url == "/admin.json") {
-    let items = [wiki.paragraph("Active meta-sites:")]
-    for (let siteName of system.metaSitesInDomain(req)) {
-      items.push(wiki.paragraph(`[http://${siteName}/view/welcome-visitors ${siteName}]`))
-    }
-    items.push(wiki.paragraph("Sites with passwords:"))
-    let count = 0
-    for (let site of Object.values(system.metaSites)) {
-      if (site.secret) {
-        count += 1
-        items.push(wiki.paragraph(`${site.name}: ${site.secret}`))
+  }
+  return sites;
+}
+
+async function doAndShare(metaSite) {
+  try {
+    let response = await fetch(`http://${metaSite}/welcome-visitors.json`);
+    let page = await response.json();
+    for (let item of page.story) {
+      if (item.id == wiki.DO_AND_SHARE_ID) {
+        return item.text;
       }
     }
-    if (count == 0) {
-      items.push(wiki.paragraph("None"))
+  } catch (e) {
+    // Note: Fetch seems unable to resolve du.localhost or region.localhost
+    console.log(`${metaSite}: Unable to get 'do and share' entry`, e);
+  }
+  // TODO: Call each meta page to get its title or use the slug
+  // Figure out how to work around meta pages with long load times
+  // if (metaSite.metaPages) {
+  //     return Object.keys(metaSite.metaPages).map((p) => `[[${p}]]`).join(", ")
+  // }
+  return "Dynamic meta-site";
+}
+
+export let handler = new wiki.Handler()
+
+handler.page(wiki.welcomePage("[[DenoWiki]]", "[[Admin]], [[Deno Sites]]"))
+handler.items("Admin", async (req: Request, system: System) => {
+  let items = []
+  items.push(wiki.paragraph("Active meta-sites:"))
+  for (let siteName of system.metaSitesInDomain(req)) {
+    items.push(wiki.paragraph(`[http://${siteName}/view/welcome-visitors ${siteName}]`))
+  }
+  items.push(wiki.paragraph("Sites with passwords:"))
+  let count = 0
+  for (let site of Object.values(system.metaSites)) {
+    if (site.secret) {
+      count += 1
+      items.push(wiki.paragraph(`${site.name}: ${site.secret}`))
     }
-    let page = wiki.page("Admin", items)
-    page.protected = true
-    wiki.serveJson(
-      req,
-      page
-    );
-  } else if (req.url == "/wander.json") {
-    wiki.serveJson(
-      req,
-      wiki.page("Wander", [wiki.item("turtle-wander", {})])
-    );
-  } else if (metaPages[req.url]) {
-    // These are meta-pages from the meta-pages folder
-    console.log("calling:", metaPages[req.url]);
-    let data = await metaPages[req.url](req, system);
-    wiki.serveJson(req, data);
-  } // This will serve system urls
-  else {
-    wiki.serve(req, system);
+  }
+  if (count == 0) {
+    items.push(wiki.paragraph("None"))
+  }
+  items.push(wiki.item("cm", {}))
+  return items
+}, { protected: true })
+
+handler.items("Deno Sites", sites)
+
+async function serve(req: Request, system: System) {
+  if (req.url.indexOf("/cm") == 0) {
+    let filetypes = {
+      "js": "text/javascript",
+      "css": "text/css"
+    }
+    let ext = req.url.split(".")[1]
+    let filetype = filetypes[ext]
+    if (!filetype) {
+      console.log("Unknown filetype:", ext, req.url)
+      Deno.exit(1)
+    }
+    wiki.serveFile(req, filetype, join("./client", req.url))
+    return true
   }
 }
