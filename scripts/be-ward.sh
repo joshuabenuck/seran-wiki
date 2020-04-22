80c62a2a43f1daef57219f417777c0352f80ea48
Ward Cunningham <ward@c2.com>
2020-04-21 18:52:25 -0700

    struggle to get escapes right

--- scripts/be-ward.sh
+++ scripts/be-ward.sh
@@ -1,4 +1,4 @@
 # revise the github automation to build for WardCunningham
 # usage: sh be-ward.sh
 
-perl -pi.bak -e 's/\$\{\{ *github.repository *\}\}/wardcunningham\seran-wiki/g' .github/workflows/dockerimage.yml
+perl -pi.bak -e 's/\$\{\{ *github.repository *\}\}/wardcunningham\/seran-wiki/g' .github/workflows/dockerimage.yml
