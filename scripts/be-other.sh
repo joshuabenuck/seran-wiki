a9f452f8ee5ee4b6974b5c60d0103f94a18d130f
Ward Cunningham <ward@c2.com>
2020-04-21 18:49:28 -0700

    proper org/repo name

--- scripts/be-other.sh
+++ scripts/be-other.sh
@@ -1,4 +1,4 @@
 # revise the github automation to build for other than WardCunningham
 # usage: sh be-other.sh
 
-perl -pi.bak -e 's/wardcunningham/${{ github.repository }}/g' .github/workflows/dockerimage.yml
+perl -pi.bak -e 's/wardcunningham\/seran-wiki/\$\{\{ github.repository \}\}/g' .github/workflows/dockerimage.yml
