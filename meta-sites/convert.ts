821e4db7f2bd696838c9dd53b95b8cc7f67f40d1
Joshua Benuck <joshua_benuck@yahoo.com>
2020-05-14 16:37:23 -0700

    fix convert meta-site, temporary hack for outposts meta-site

--- meta-sites/convert.ts
+++ meta-sites/convert.ts
@@ -1,41 +1,36 @@
 import * as wiki from "seran/wiki.ts";
 import { Request } from "seran/wiki.ts";
 
+export let handler = new wiki.Handler()
+
 let conversions = {
   "inches-to-millimeters": (inches) => inches * 25.4,
   "millimeters-to-inches": (millimeters) => millimeters / 25.4
 };
 
-export async function serve(req: Request, system) {
-  if (req.url == "/welcome-visitors.json") {
-    await wiki.serveJson(
-      req,
-      wiki.welcomePage("[[DenoWiki]]", "[[Inches to Millimeters]]")
-    );
-  } else if (req.url.match(/\/.*-to-.*\.json/)) {
-    let conversion = req.url.replace(".json", "").replace("/", "");
-    let [src, dst] = conversion.split("-to-");
-    let formula = conversions[conversion];
-    if (!formula) {
-      await wiki.serveJson(
-        req,
-        wiki.page(
-          "Unknown",
-          [ wiki.paragraph(`This site is unable to convert '${src}' to '${dst}'`) ]
-        )
-      );
-      return;
-    }
-    src = src.charAt(0).toUpperCase() + src.substring(1);
-    dst = dst.charAt(0).toUpperCase() + dst.substring(1);
+handler.page(wiki.welcomePage("[[DenoWiki]]", "[[Inches to Millimeters]]"))
+handler.route(".*-to-.*\.json", async (req) => {
+  let conversion = req.url.replace(".json", "").replace("/", "");
+  let [src, dst] = conversion.split("-to-");
+  let formula = conversions[conversion];
+  if (!formula) {
     await wiki.serveJson(
       req,
       wiki.page(
-        `${src} to ${dst}`,
-        [wiki.paragraph(`${src}: 1`), wiki.paragraph(`${dst}: ${formula(1)}`)]
+        "Unknown",
+        [ wiki.paragraph(`This site is unable to convert '${src}' to '${dst}'`) ]
       )
     );
-  } else {
-    await wiki.serve(req, system);
+    return;
   }
-}
+  src = src.charAt(0).toUpperCase() + src.substring(1);
+  dst = dst.charAt(0).toUpperCase() + dst.substring(1);
+  await wiki.serveJson(
+    req,
+    wiki.page(
+      `${src} to ${dst}`,
+      [wiki.paragraph(`${src}: 1`), wiki.paragraph(`${dst}: ${formula(1)}`)]
+    )
+  );
+  return true
+});
